6a6d6d55b0cde4c0452f7de43ea21e6c324feb3d
Sheckerol <checol70@gmail.com>
2018-09-17 16:45:14 -0700

    added initial unity build

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const gameRoutes = require("./routes/gameRoutes")
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+
+
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
